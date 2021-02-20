69bd6d2d30e252542215b7d216e19f5d8ae16de1
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2021-02-19 19:18:18 -0600

    feat: update button styles

--- app/src/main/java/com/gilbertohdz/LoadingButton.kt
+++ app/src/main/java/com/gilbertohdz/LoadingButton.kt
@@ -2,9 +2,12 @@ package com.gilbertohdz
 
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import com.gilbertohdz.utils.dpToPx
+import com.gilbertohdz.utils.spToPx
+import java.util.*
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -15,19 +18,93 @@ class LoadingButton @JvmOverloads constructor(
 
     private val valueAnimator = ValueAnimator()
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    private val cornerPath = Path()
+    private val circleRect = RectF()
+    private val cornerRadius = 4.dpToPx().toFloat()
 
-    }
+    private var textColor = Color.WHITE
+    private var primaryBackgroundColor = context.getColor(R.color.colorPrimary)
+    private var secondaryBackgroundColor = context.getColor(R.color.colorPrimaryDark)
+    private var circularProgressColor = context.getColor(R.color.colorAccent)
 
 
     init {
 
+        context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.LoadingButton,
+                defStyleAttr,
+                0
+        ).apply {
+            textColor = getColor(
+                    R.styleable.LoadingButton_textColor,
+                    textColor
+            )
+            primaryBackgroundColor = getColor(
+                    R.styleable.LoadingButton_primaryBackgroundColor,
+                    primaryBackgroundColor
+            )
+            secondaryBackgroundColor = getColor(
+                    R.styleable.LoadingButton_secondaryBackgroundColor,
+                    secondaryBackgroundColor
+            )
+            circularProgressColor = getColor(
+                    R.styleable.LoadingButton_circularProgressColor,
+                    circularProgressColor
+            )
+
+            recycle()
+        }
+    }
+
+    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+
+        when (buttonState) {
+            ButtonState.Clicked -> {
+
+            }
+            ButtonState.Loading -> {
+
+            }
+            ButtonState.Completed -> {
+
+            }
+        }
     }
 
 
+    private val textRect = Rect()
+    private var textToDraw = context.getString(R.string.download).toUpperCase(Locale.ENGLISH)
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        canvas?.let {
+            it.save()
 
+            // Clip canvas corners to form a rounded button
+            it.clipPath(cornerPath)
+
+            // Draw button background color
+            it.drawColor(primaryBackgroundColor)
+
+            // Retrieve button text bounds
+            textPaint.getTextBounds(textToDraw, 0, textToDraw.length, textRect)
+            val centerX = measuredWidth.toFloat() / 2 - textRect.centerX()
+            val centerY = measuredHeight.toFloat() / 2 - textRect.centerY()
+
+            // Draw button text
+            textPaint.color = textColor
+            it.drawText(textToDraw, centerX, centerY, textPaint)
+
+            // Restore saved canvas
+            it.restore()
+        }
+    }
+
+    private val textPaint = Paint().apply {
+        isAntiAlias = true
+        textAlignment = TEXT_ALIGNMENT_CENTER
+        textSize = 16.spToPx().toFloat()
+        typeface = Typeface.DEFAULT_BOLD
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -43,4 +120,18 @@ class LoadingButton @JvmOverloads constructor(
         setMeasuredDimension(w, h)
     }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        cornerPath.reset()
+        cornerPath.addRoundRect(
+                0f,
+                0f,
+                w.toFloat(),
+                h.toFloat(),
+                cornerRadius,
+                cornerRadius,
+                Path.Direction.CW
+        )
+        cornerPath.close()
+    }
 }
